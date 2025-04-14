da640b6aba16143667d20a3c7197eabbd7e12b1c
prince-ravi-leow <princeravileow@outlook.com>
2025-04-14 23:17:32 +0200

    removed comments from various installation scripts

--- ffrotate_app.py
+++ ffrotate_app.py
@@ -1,9 +1,9 @@
 import webview
 
-from ffrotate import ffrotate_app  # CHANGE THIS LINE
+from ffrotate import ffrotate_app  
 
 ffrotate_app.launch(prevent_thread_lock=True)
 webview.settings['ALLOW_DOWNLOADS'] = True
 
-webview.create_window("ffrotate", ffrotate_app.local_url)  # Change the title if needed
+webview.create_window("ffrotate", ffrotate_app.local_url)  
 webview.start()
